561f8e4b57aea02cb5d19e01d2b41165ca203d48
Fabien <tambass85@gmail.com>
2020-09-19 07:48:04 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+var path = require('path');
+const app = express();
+const port = 3000;
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+//setup public folder
+app.use(express.static('./public'));
+app.get('/',function (req, res) {
+res.render('admin_home_page', {title: "Bienvenue"})
+});
+app.listen(port, () => {
+    console.log('Le server écoute le port : ', port);
+});
