acd99aa8fd7686fe2cea1cf997bfb150246c2577
Fabien <tambass85@gmail.com>
2020-09-19 09:25:57 +0200

    custom header.ejs with navbar & sidebar

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 var path = require('path');
 const app = express();
-const port = 3000;
+const port = 1987;
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
